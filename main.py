e1d75bb1819e11ec563a4feb2e9066551058fe60
AlanKhub <alan_hub@mail.ru>
2024-09-08 13:58:05 +0500

    finished developing upgrades

--- main.py
+++ main.py
@@ -14,8 +14,12 @@ ret_angle = 0
 bullet_x = 0
 bullet_y = 0
 bg_y = 0
+damage_multiplier = 1
+speed_multiplier = 1
 bullets = []
 enemies = []
+upgrades = []
+
 class Player(object):
     def __init__(self, x, y, width, height):
         self.x = x
@@ -30,19 +34,33 @@ class Player(object):
         self.jumpCount = 10
         self.standing = True
         self.lives = 3
+
     def draw(self, win):
         if self.walkCount + 1 >= 27:
             self.walkCount = 0
         screen.blit(player_img, (self.x, self.y))
 
+class Upgrades(object):
+    def __init__(self, x, y, width, height, type):
+        self.x = x
+        self.y = y
+        self.vel = 2
+        self.width = width
+        self.height = height
+        self.type = type
+        self.img = upgrade_img[self.type]
+
+    def draw(self, win):
+        screen.blit(self.img, (self.x, self.y))
 
 class Projectile(object):
     def __init__(self, x, y, width, height):
+        global speed_multiplier
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        self.vel = 10
+        self.vel = 5 * speed_multiplier
 
     def draw(self, win):
         screen.blit(bullet_img, (self.x, self.y))
@@ -54,15 +72,16 @@ class Enemy(object):
         self.width = width
         self.height = height
         self.vel = 4
-        self.lives = 1 + level//10
+        self.lives = 1 + level // 10
         if self.lives > 5:
             self.lives = 5
         self.enemyimg = enemy_img[self.lives - 1]
+
     def draw(self, win):
         screen.blit(self.enemyimg, (self.x, self.y))
 
 def redrawGameWindow(bg_y):
-    screen.blit(background, (0, bg_y - screen_height))  # screen.fill((0, 255, 0))
+    screen.blit(background, (0, bg_y - screen_height))
     if bg_y > 0:
         screen.blit(background, (0, bg_y))
 
@@ -71,74 +90,84 @@ def redrawGameWindow(bg_y):
         bullet.draw(screen)
     for enemy in enemies:
         enemy.draw(screen)
-    text_screen = font.render(text + str(score), True, (0, 0, 0))
-    lives_text_screen = font.render("Lives: " + "♥"*player.lives, True, (255, 0, 0))
-    level_text_screen = font.render("level: " + str(level), True, (0, 0, 0))
-    screen.blit(text_screen, (0, 0))
-    screen.blit(lives_text_screen, (0, 30))
+    for upgrade in upgrades:
+        upgrade.draw(screen)
+
+    text_screen = font.render(text + str(score), True, (150, 200, 255))
+    shield_text_screen = font.render(text_shield, True, (150, 200, 255))
+    stop_text_screen = font.render(text_stop, True, (150, 200, 255))
+    lives_text_screen = font.render("Lives: " + "♥" * player.lives, True, (255, 0, 0))
+    level_text_screen = font.render("level: " + str(level), True, (150, 200, 255))
+
+    screen.blit(text_screen, (0, 30))
+    screen.blit(lives_text_screen, (0, 0))
     screen.blit(level_text_screen, (0, 60))
+    screen.blit(shield_text_screen, (0, 90))
+    screen.blit(stop_text_screen, (0, 120))
     pygame.display.update()
 
 ignore_list = []
 
-pygame.init() # запуск
+pygame.init()
 player_ydir = 0
 player_xdir = 0
 counter = 0
-background=pygame.image.load("Backgrounds/background1.png")
+background = pygame.image.load("Backgrounds/background1.png")
 font = pygame.font.Font('freesansbold.ttf', 32)
 text = "Score: "
+text_shield = "SHIELD time left: "
+text_stop = "STOP time left: "
 screen_height = background.get_height()
 screen_width = background.get_width()
 screen = pygame.display.set_mode((length, width))
 super_distance = 0
 score = 0
-level = 100
+level = 29
 enemies_num = 10
 enemies_present = 0
 time_interval = 20
+time_shield = 0
+shield = False
+time_stop = 0
+stop = False
 player_img = pygame.image.load("PNG/playerShip2_red.png")
-player_img_master = pygame.image.load("PNG/playerShip2_red.png").convert()
 enemy_img = [pygame.image.load("PNG/Enemies/enemyBlack1.png"), pygame.image.load("PNG/Enemies/enemyRed1.png"), pygame.image.load("PNG/Enemies/enemyYellow1.png"), pygame.image.load("PNG/Enemies/enemyGreen1.png"), pygame.image.load("PNG/Enemies/enemyBlue1.png")]
+upgrade_img = [pygame.image.load("PNG/Upgrades/double_bullet.png"), pygame.image.load("PNG/Upgrades/speed_bullet.png"), pygame.image.load("PNG/Upgrades/super_lives.png"), pygame.image.load("PNG/Upgrades/super_shield.png"), pygame.image.load("PNG/Upgrades/time_stop.png")]
 bullet_img = pygame.image.load("PNG/Lasers/laserRed15.png").convert()
 bullet_img_master = pygame.image.load("PNG/Lasers/laserRed15.png").convert()
 player = Player(844, 470, 112, 75)
 screen = pygame.display.set_mode((length, width))
 cond = 1
-#bullet.rect.y = 500000000
-#bullet.rect.x = 500000000
 time_start = time.time()
+
 while 1:
     clock.tick(144)
     if cond == 1:
         for enemy in enemies:
-            enemy.vel = 4 + level//10
+            enemy.vel = 4 + level // 10
             if enemy.vel > 10:
                 enemy.vel = 10
-        enemies_num = 10 + 2*(level - 1)
+        enemies_num = 10 + 2 * (level - 1)
         if enemies_num > 100:
             enemies_num = 100
-        time_interval = 5 - level/2
+        time_interval = 5 - level / 2
         if time_interval < 0:
             time_interval = 0.5
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 break
-            elif event.type == pygame.KEYDOWN:  # если нажата клавиша
+            elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     if len(bullets) < 3:
                         bullets.append(Projectile(round(player.x + player.width // 2 - 5), round(player.y + player.height // 2 - 50), 9, 37))
-                #if event.key == pygame.K_z:
-                #    if len(enemies) < 10000:
-                #        enemies.append(Enemy(random.randint(50, 1638), 5, 93, 84, enemy_img[random.randint(0, len(enemy_img)-1)]))
+
         if time.time() - time_start >= time_interval:
             time_start = time.time()
             if enemies_present < enemies_num:
-                enemies.append(
-                    Enemy(random.randint(50, 1638), 5, 93, 84))
+                enemies.append(Enemy(random.randint(50, 1638), 5, 93, 84))
                 enemies_present += 1
             if len(enemies) == 0 and enemies_present == enemies_num:
-                level+=1
+                level += 1
                 print("level " + str(level) + " began!")
                 enemies_present = 0
 
@@ -174,62 +203,100 @@ while 1:
             player.y = 650
         if player.y > 870:
             player.y = 870
+
         for bullet in bullets:
             if bullet.y < 1000 and bullet.y > 0:
                 bullet.y -= bullet.vel
             else:
                 bullets.pop(bullets.index(bullet))
 
+        for upgrade in upgrades:
+            if upgrade.y < 1000 and upgrade.y > 0:
+                upgrade.y += upgrade.vel
+            elif upgrade.y > 1000:
+                upgrades.pop(upgrades.index(upgrade))
+            upg_distance = math.sqrt((player.x - upgrade.x) ** 2 + (player.y - upgrade.y) ** 2)
+            if upg_distance < 60:
+                if upgrade.type == 0:
+                    damage_multiplier = 2
+                elif upgrade.type == 1:
+                    speed_multiplier = 2
+                elif upgrade.type == 2:
+                    player.lives += 3
+                elif upgrade.type == 3:
+                    shield = True
+                    time_shield = time.time()
+                    player_img = pygame.image.load("PNG/playerShip2_red_shield.png")
+                elif upgrade.type == 4:
+                    stop = True
+                    time_stop = time.time()
+                upgrades.pop(upgrades.index(upgrade))
+
+        if shield:
+            #print(int(time.time() - time_shield))
+            text_shield = "SHIELD time left: " + "♥"*(10 - int(time.time() - time_shield))
+            if time.time() - time_shield >= 10:
+                shield = False
+                player_img = pygame.image.load("PNG/playerShip2_red.png")
+        else:
+            text_shield = ""
+        if stop:
+            text_stop = "TIME STOP left: " + "♥"*(10 - int(time.time() - time_stop))
+            if time.time() - time_stop >= 10:
+                for enemy in enemies:
+                    enemy.vel = 4
+                stop = False
+            else:
+                for enemy in enemies:
+                    enemy.vel = 0.5
+        else:
+            text_stop = ""
+
+        enemies_to_remove = []
         for enemy in enemies:
             if enemy.y < 1000 and enemy.y > 0:
                 enemy.y += enemy.vel
             else:
                 enemy.y = 5
                 enemy.x = random.randint(50, 1638)
+
             for bullet in bullets:
-                super_distance = math.sqrt(
-                    (bullet.x - enemy.x) ** 2 + (bullet.y - enemy.y) ** 2)
+                super_distance = math.sqrt((bullet.x - enemy.x) ** 2 + (bullet.y - enemy.y) ** 2)
                 if super_distance < 90:
-                    enemy.lives -= 1
-                    enemy.enemyimg = enemy_img[enemy.lives - 1]
+                    if enemy.lives >= damage_multiplier:
+                        enemy.lives -= 1 * damage_multiplier
+                        enemy.enemyimg = enemy_img[enemy.lives - 1]
+                    else:
+                        enemy.lives = 0
                     bullets.pop(bullets.index(bullet))
                     if enemy.lives == 0:
                         score += 1
-                        if enemies.index(enemy) in ignore_list:
-                            ignore_list.pop(id(enemy))
-                        enemies.pop(enemies.index(enemy))
-                    if score%15 == 0 and score != 0:
-                        player.lives += 1
+                        enemies_to_remove.append(enemy)
+                        if score % 5 == 0 and score != 0:
+                            upgrades.append(Upgrades(random.randint(0, 1688), 10, 40, 40, random.randint(0, 4)))
+                        if score % 15 == 0 and score != 0:
+                            player.lives += 1
+
             dead = math.sqrt((player.x - enemy.x) ** 2 + (player.y - enemy.y) ** 2)
             if dead <= 35:
-                player.lives -= 1
+                if not shield:
+                    player.lives -= 1
                 enemy.lives -= 1
-                if id(enemy) in ignore_list:
-                    player.lives += 1
-                    enemy.lives += 1
-                else:
-                    ignore_list.append(id(enemy))
                 if enemy.lives == 0:
-                    if enemies.index(enemy) in ignore_list:
-                        try:
-                            ignore_list.pop(id(enemy))
-                        except Exception:
-                            pass
-                    enemies.pop(enemies.index(enemy))
+                    enemies_to_remove.append(enemy)
                 if player.lives == 0:
                     background = pygame.image.load("Backgrounds/lost.png")
                     cond = 2
-            else:
-                if id(enemy) in ignore_list and len(ignore_list):
-                    #print(len(ignore_list))
-                    try:
-                        ignore_list.pop(id(enemy))
-                    except Exception:
-                        pass
+
+        for enemy in enemies_to_remove:
+            if enemy in enemies:
+                enemies.pop(enemies.index(enemy))
+
         bg_y += 1
-        if bg_y >= screen_height:  # дест = destination чтобы фон двигался
+        if bg_y >= screen_height:
             bg_y = 0
         redrawGameWindow(bg_y)
+
     elif cond == 2:
         for enemy in enemies:
             enemies.pop(enemies.index(enemy))
@@ -238,7 +305,7 @@ while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 break
-            elif event.type == pygame.KEYDOWN:  # если нажата клавиша
+            elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_r:
                     cond = 1
                     background = pygame.image.load("Backgrounds/background1.png")
